7a4d0bd16e9bb7f10ff29496ce225196679cbd14
oliviergarcia <oliligarcia@yahoo.fr>
2024-09-25 17:39:19 +0200

    downgrade react dependency

--- src/extensions/utils/routeUtils.ts
+++ src/extensions/utils/routeUtils.ts
@@ -4,7 +4,7 @@ export const getOpenExternalPageHandler = (url: string, target: '_blank' | '_sel
     return () => {console.log("external link is empty")}; 
   
   if (target === '_blank') 
-    return() => {window.open(url, "_blank")};
+    return () => {window.open(url, "_blank")};
   
   return () => {document.location.href = `${url}`};
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client'; // for react 18 in the future ?
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import {
@@ -24,8 +25,9 @@ import 'bootstrap/dist/js/bootstrap.bundle';
 import App from './App';
 
 const localeURL = process.env.REACT_APP_CONTENT_URL + '/locales';
-const defaultLanguage = process.env.REACT_APP_DEFAULT_LANGUAGE ?? process.env.REACT_APP_FALLBACK_LANGUAGE ?? 'en';
+const defaultLanguage = process.env.REACT_APP_DEFAULT_LANGUAGE ?? process.env.REACT_APP_FALLBACK_LANGUAGE ?? 'fr';
 initI18n(defaultLanguage, defaultLanguage, localeURL);
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -41,6 +43,23 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// FOR REACT 18 : use createRoot instead of ReactDom.render But :
+// 1. @influenzanet:case-web-ui and case-web-app-core ask react 17
+// 2. component symptomHistory won't work with this change !
+// const container = document.getElementById('root');
+// const root = createRoot(container!);
+// root.render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <Suspense fallback={<LoadingPlaceholder
+//         color="secondary"
+//         minHeight="100vh"
+//       />}>
+//         <App />
+//       </Suspense>
+//     </Provider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
